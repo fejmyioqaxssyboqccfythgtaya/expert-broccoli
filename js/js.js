f1477e8f73421fe3eac1e6acabc614fcfc830f82
fejmyioqaxssyboqccfythgtaya <fejmyioqaxssyboqccfythgtaya@gmail.com>
2021-12-21 21:53:51 -0300

    Alterações ate Parte 8

--- js/js.js
+++ js/js.js
@@ -10,10 +10,52 @@ function start() { // Inicio da função start()
   
   //Principais variáveis do jogo
   var jogo = {}
+	var TECLA = {
+      W: 87,
+      S: 83,
+      D: 68
+    }
+  
+  jogo.pressionou = [];
+
+  //Verifica se o usuário pressionou alguma tecla	
+	$(document).keydown(function(e){
+    jogo.pressionou[e.which] = true;
+  });
+  
+  
+  $(document).keyup(function(e){
+    jogo.pressionou[e.which] = false;
+  });
+
+  function movejogador() {
+    if (jogo.pressionou[TECLA.W]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top",topo-10);
+    }
+    
+    if (jogo.pressionou[TECLA.S]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top",topo+10);	
+    }
+    
+    if (jogo.pressionou[TECLA.D]) {
+      
+      //Chama função Disparo	
+    }
+  
+} // fim da função movejogador()
+  
+
+
+
+
+
   //Game Loop
   jogo.timer = setInterval(loop,30);
   function loop() {
     movefundo();
+    movejogador();
   } // Fim da função loop()
 
 
@@ -24,7 +66,7 @@ function start() { // Inicio da função start()
     $("#fundoGame").css("background-position",esquerda-1);
   } // fim da função movefundo()
 
-  
+
 
 } // Fim da função start
 
